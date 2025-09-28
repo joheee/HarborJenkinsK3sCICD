262605c9bf5a743cb52b4240ec7937e2db92c7f1
joheee <johevinblesstowi07@gmail.com>
2025-09-29 02:38:21 +0700

    checking: without the deployment and service inside kubernetes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>seriusan bisa ga nih bang</h1>
+      <h1>testing without initial deployment and service</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
